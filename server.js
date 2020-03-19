2ebe6bb3580fff0033a3699e3d4cc648c671b36a
jayballa7 <Jenn_Ballard7@hotmail.com>
2020-03-18 18:35:29 -0700

    deploy to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const PORT = 3030;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: true }));
